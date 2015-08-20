3caa89c2ee55dcd184ec40aab84c3dfb24dfe976
Martino Colombo <martino.colombo8@gmail.com>
2015-08-20 18:29:33 +0200

    knowns.gtf and trans_exons

--- get_hairpins.py
+++ get_hairpins.py
@@ -1,1 +1,15 @@
-__author__ = 'martin'
+import parsers as ps
+import functions as fn
+
+gtf_parser = ps.Gtf('/Users/martin/Dropbox/utils/drosophila/Drosophila_melanogaster.BDGP6.81toy.gtf')
+
+trans_exons = gtf_parser.get_trans_exon(annot='.')
+trans_exons_manager = fn.TransExons(trans_exons)
+trans_start_stops = gtf_parser.get_start_stop_codons(annot='.')
+
+trans_rel_start_stops = {}
+for trans in trans_exons.keys():
+    if trans in trans_start_stops:
+        (start, stop) = trans_start_stops[trans]
+        trans_rel_start_stops[trans] = trans_exons_manager.rel_pos_trans(trans, [start[0], stop[0]])
+
